a598402975d4c712c90a66e1441ce0ab38a4e8ac
kiart-tantasi <petch_rwb@hotmail.com>
2021-12-26 01:47:21 +0700

    PORT instead of port

--- app.js
+++ app.js
@@ -263,5 +263,5 @@ function blockAuthenticated(req,res,next) {
 }
 
 
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log("running on", port));
