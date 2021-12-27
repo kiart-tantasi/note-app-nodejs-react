0ec7b0a9f918663133d0e7c06f7227a496689756
kiart-tantasi <petch_rwb@hotmail.com>
2021-12-27 17:49:27 +0700

    homepage static

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 // const cors = require("cors");
 // const MongoStore = require("connect-mongo");
 const path = require("path");
-app.use(express.static(path.resolve(__dirname, "./build"))); // homepage
+app.use(express.static(path.join(__dirname, "build"))); //homepage
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 // app.use(
