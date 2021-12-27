df251a10c0644eb647790f76a89fcb3ed2bb76bd
kiart-tantasi <petch_rwb@hotmail.com>
2021-12-27 17:47:22 +0700

    maxAge to 7 days

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: {maxAge: 60*60*1000} 
+    cookie: {maxAge: 7*24*60*60*1000} 
 }))
 app.use(passport.initialize());
 app.use(passport.session());
